5d8c76d5a58fc518a9253267683d20c682333033
Martin Wang <ms0695961@gmail.com>
2021-06-07 14:06:51 +0800

    add random_guess python code

---
+++ r.py
@@ -0,0 +1,24 @@
+#Generate ramdom integers (1~100)
+#Let user to guess the number
+#If right, print "Good Job"
+#If wrong, tell them it is bigger or smaller than anwser.
+
+import random
+
+r = random.randint(1, 10)
+#print(r)
+
+
+
+while True:
+	num = input('Please enter your anwser: ')
+	num = int(num)
+	if num == r:
+		print('Good Job')
+		break
+	elif num > r:
+		#print(r)
+		print('It is too big!! Try again')
+	elif num < r:
+		#print(r)
+		print('It is too small!! Try again')
